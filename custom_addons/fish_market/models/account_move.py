ac0f0bba5508a822e87281d0efa6f4770d5d30ca
Ubuntu <ubuntu@ip-172-31-39-237.af-south-1.compute.internal>
2024-01-14 19:16:23 +0000

    Finishing Invoices

--- custom_addons/fish_market/models/account_move.py
+++ custom_addons/fish_market/models/account_move.py
@@ -10,3 +10,12 @@ class SaleOrder(models.Model):
     seal_number = fields.Char('Seal Number')
     driver_name = fields.Char('Driver Name')
     telephone_number = fields.Char('Telephone Number')
+
+    alternative_currency_amount_total = fields.Monetary('Alternative Total Price', currency_field='currency_id', compute='_compute_alternative_currency_amount_total')
+
+    @api.depends('amount_total', 'currency_id')
+    def _compute_alternative_currency_amount_total(self):
+        for record in self:
+            print('_compute_alternative_currency_amount_total', record.currency_id, record.company_id.currency_id)
+            if record.currency_id.id != record.company_id.currency_id.id:
+                record.alternative_currency_amount_total = record.amount_total * record.currency_id.rate
