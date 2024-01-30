e904df48c697cc8d5b9bb807a00ebda363b9e0ed
Ubuntu <ubuntu@ip-172-31-39-237.af-south-1.compute.internal>
2024-01-30 07:48:55 +0000

    Adding Buggy Bank Reconciliation: account_reconcile_oca/account_statement_base

---
+++ appstore_addons/account_reconcile_oca/static/src/js/widgets/reconcile_chatter_field.esm.js
@@ -0,0 +1,17 @@
+/** @odoo-module **/
+
+// import {ChatterContainer} from "@mail/components/chatter_container/chatter_container";
+import { Chatter } from '@mail/core/web/chatter';
+
+import {registry} from "@web/core/registry";
+
+const {Component} = owl;
+
+export class AccountReconcileChatterWidget extends Component {}
+AccountReconcileChatterWidget.template =
+    "account_reconcile_oca.AccountReconcileChatterWidget";
+AccountReconcileChatterWidget.components = {...Component.components, Chatter};
+
+registry
+    .category("fields")
+    .add("account_reconcile_oca_chatter", AccountReconcileChatterWidget);
