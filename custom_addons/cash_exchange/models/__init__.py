35e1e5630b25e8b1172415e7c05476ab551eaf80
Ubuntu <ubuntu@ip-172-31-39-237.af-south-1.compute.internal>
2024-02-05 17:48:44 +0000

    Removing account.account.currency.exchange and account.journal.currency.exchange

--- custom_addons/cash_exchange/models/__init__.py
+++ custom_addons/cash_exchange/models/__init__.py
@@ -2,5 +2,4 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from . import account_account
-from . import account_account_currency_exchange
 from . import account_payment
