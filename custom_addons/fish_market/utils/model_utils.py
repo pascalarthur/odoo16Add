965a786175b254d3e9f4fb4466e9d901527a2eb4
Ubuntu <ubuntu@ip-172-31-41-47.af-south-1.compute.internal>
2024-02-21 16:51:47 +0000

    Moving damages to own module product_damaged

--- custom_addons/fish_market/utils/model_utils.py
+++ custom_addons/fish_market/utils/model_utils.py
@@ -1,6 +1,6 @@
 def default_name(obj, prefix: str) -> str:
     last_order = obj.search([], order='id desc', limit=1)
-    if last_order and last_order.name.startswith(prefix) and last_order.name[len(prefix):].isdigit():
+    if last_order.name and last_order.name.startswith(prefix) and last_order.name[len(prefix):].isdigit():
         last_number = int(last_order.name[len(prefix):])
         new_number = last_number + 1
     else:
---
+++ custom_addons/product_damaged/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': "Inventory Damaged Products",
+    'version': '1.0',
+    'depends': ['base', 'product', 'sale_management', 'purchase', 'stock', 'account', 'sale_stock'],
+    'author': "Ludwig Gräf",
+    'category': 'Inventory/Inventory',
+    'description': """
+    Convert products to damaged products.
+    """,
+    # data files always loaded at installation
+    'data': [
+        'security/ir.model.access.csv',
+        'data/product_damages_sequence.xml',
+        'views/stock_picking.xml',
+        'views/damage_views.xml',
+        'views/menu.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3'
+}
