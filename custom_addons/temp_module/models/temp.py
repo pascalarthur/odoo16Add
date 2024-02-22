f7457d4b8920437213f47655b070ab35a0312308
Ubuntu <ubuntu@ip-172-31-41-47.af-south-1.compute.internal>
2024-02-22 17:28:41 +0000

    Minor changes on temp module

--- custom_addons/temp_module/models/temp.py
+++ custom_addons/temp_module/models/temp.py
@@ -1,19 +1,1 @@
 from odoo import models, fields, api, _, exceptions
-
-
-# class TruckLoadLine(models.Model):
-#     _name = "truck.detail.line"
-
-
-
-# class TruckDetail(models.Model):
-#     _name = "truck.detail"
-
-# class Damage(models.Model):
-#     _name = 'inventory.damage'
-#     _description = 'Records Damages to boxes'
-
-
-# class DamageOperation(models.Model):
-#     _name = 'inventory.damage.operation'
-#     _description = 'Records Damages to boxes'
