a6424991c666b10e1be1295749361c397537b494
Ubuntu <ubuntu@ip-172-31-4-137.af-south-1.compute.internal>
2024-02-25 12:20:06 +0000

    Removing Return model of bi_inter_company_transfer

--- appstore_addons/bi_inter_company_transfer/models/__init__.py
+++ appstore_addons/bi_inter_company_transfer/models/__init__.py
@@ -1,5 +1,4 @@
 from . import resconfigsettings
 from . import int_company_transfer
-from . import return_inter_company_transfer
 from . import sale_order_inherit
 from . import purchase_order_inherit
