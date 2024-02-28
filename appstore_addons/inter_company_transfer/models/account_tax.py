d294fa8dc9128e685e5973ef164327283736f9a2
Ubuntu <ubuntu@ip-172-31-41-47.af-south-1.compute.internal>
2024-02-28 10:52:30 +0000

    Add docstring

--- appstore_addons/inter_company_transfer/models/account_tax.py
+++ appstore_addons/inter_company_transfer/models/account_tax.py
@@ -5,6 +5,11 @@ class AccountTax(models.Model):
     _inherit = 'account.tax'
 
     def map_tax_ids(self, company) -> list:
+        """
+        Map the taxes of the current company to company. The matching is by name.
+        :param company: company object
+        :return: list of taxes
+        """
         taxes_return = self.env['account.tax']
         for tax in self:
             taxes_return += self.env['account.tax'].search([('company_id', '=', company.id), ('name', '=', tax.name)])
