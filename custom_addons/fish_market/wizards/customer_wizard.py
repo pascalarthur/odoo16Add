184337f99c61014b28c888a5758a5957192da5da
Ubuntu <ubuntu@ip-172-31-41-47.af-south-1.compute.internal>
2024-03-21 12:03:49 +0000

    Improving Email to Customers

--- custom_addons/fish_market/wizards/customer_wizard.py
+++ custom_addons/fish_market/wizards/customer_wizard.py
@@ -1,6 +1,16 @@
 from odoo import api, models, fields, exceptions
-from datetime import datetime
-from urllib.parse import urlparse
+
+
+class CustomerPricelistWizardLine(models.TransientModel):
+    _name = 'customer.price.wizard.line'
+    _description = 'Pricelist Selection Wizard Line'
+
+    customer_price_wizard_id = fields.Many2one('customer.price.wizard', string='Wizard')
+    product_id = fields.Many2one('product.product', string='Products')
+    name = fields.Char(string='Product', related='product_id.display_name')
+    quantity = fields.Float(string='Quantity')
+    currency_id = fields.Many2one('res.currency', string='Currency')
+    price = fields.Monetary(string='Price', currency_field='currency_id')
 
 
 class CustomerPricelistWizard(models.TransientModel):
@@ -11,7 +21,27 @@ class CustomerPricelistWizard(models.TransientModel):
     pricelist_id = fields.Many2one('product.pricelist', string='Pricelist', required=True)
     location_id = fields.Many2one('stock.location', string='Location', required=True)
 
+    product_ids = fields.One2many('customer.price.wizard.line', 'customer_price_wizard_id', string='Products')
+
     email_body = fields.Html(string='Email Body', readonly=False)
+
+    @api.onchange('pricelist_id', 'location_id')
+    def _compute_products(self):
+        for record in self:
+            record.product_ids = False
+            for item in record.pricelist_id.item_ids:
+                quants = self.env['stock.quant'].search([('product_id', '=', item.product_id.id),
+                                                         ('location_id', '=', record.location_id.id)])
+                quantity = sum(quants.mapped('quantity'))
+                if quantity > 0:
+                    Line = self.env['customer.price.wizard.line']
+                    record.product_ids |= Line.create({
+                        'customer_price_wizard_id': record.id,
+                        'product_id': item.product_id.id,
+                        'quantity': quantity,
+                        'currency_id': record.pricelist_id.currency_id.id,
+                        'price': item.fixed_price
+                    })
 
     def compute_email_body(self):
         for record in self:
@@ -24,17 +54,12 @@ class CustomerPricelistWizard(models.TransientModel):
                     <th style='border: 1px solid black;'>Available Quantity</th>
                 </tr>
             """
-            for item in record.pricelist_id.item_ids:
-                product_id = item.product_tmpl_id.product_variant_id
-                # Get the quantity available in the specified location
-                quants = self.env['stock.quant'].search([('product_id', '=', product_id.id),
-                                                         ('location_id', '=', record.location_id.id)])
-                quantity = sum(quants.mapped('quantity'))
+            for item in record.product_ids:
                 email_body += f"""
                 <tr>
-                    <td style='border: 1px solid black;'>{product_id.name}</td>
-                    <td style='border: 1px solid black;'>{item.fixed_price}</td>
-                    <td style='border: 1px solid black;'>{quantity}</td>
+                    <td style='border: 1px solid black;'>{item.name}</td>
+                    <td style='border: 1px solid black;'>{item.price} {item.currency_id.symbol}</td>
+                    <td style='border: 1px solid black;'>{item.quantity}</td>
                 </tr>
                 """
             record.email_body = email_body + "</table>"
